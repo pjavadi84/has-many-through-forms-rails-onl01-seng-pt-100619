f75f7f90c7fcc3dd3e24e60c261932730946dd0e
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-03-18 00:40:04 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accept_nested_attributes_for :categories
 
 
   def categories_attributes=(category_attributes)
